ba2e0286342c69279ab78d7e95f4466403d019ab
Alan Geirnaert <geirnaert.alan@icloud.com>
2025-07-28 23:03:42 +0200

    core: installed mongodb

---
+++ lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = { appName: "alan.globuddy" };
+
+let client: MongoClient;
+
+if (process.env.NODE_ENV === "development") {
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
+} else {
+  client = new MongoClient(uri, options);
+}
+
+export default client;
+export const oid = (id: string) => new ObjectId(id);
