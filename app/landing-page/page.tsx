0cb983749e6741d337124834adecc99a874b05a2
Alan Geirnaert <geirnaert.alan@icloud.com>
2025-07-29 10:29:47 +0200

    fix: css + title

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -5,11 +5,21 @@ import { useRouter } from "next/navigation";
 export default function LandingPage() {
     const router = useRouter();
 
+    const buttonsList = [
+      { name: 'Register', value: 'register' },
+      { name: 'Login', value: 'login' },
+    ];
+
     return (
         <>
-          <div className="flex flex-col min-h-screen bg-gradient-to-r from-[#224aa8] to-[#b56fdc]">
-            <header>Hi</header>
+          <div className="flex flex-col items-center justify-center min-h-screen text-background bg-gradient-to-r from-[#224aa8] to-[#b56fdc]">
+            <header className="text-8xl">Globuddy</header>
+            <main>
+              {buttonsList.map((buttonItem, index) => (
+                <button key={index}>{buttonItem.name}</button>
+              ))}
+            </main>
           </div>
         </>
     );
-}
+} 
